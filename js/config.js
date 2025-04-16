917ac3cba4c27ac436056a1a9f21fa0a39424073
zkinhang <zkinhang714@gmail.com>
2025-04-16 20:02:57 +0800

    try data analy v3

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
-import { trackPageView, trackVisitor } from './vercelAnalytics';
+
 // Configure the api
 
 const config = {
     apiKey: '8d895e74c7e2e3a659f2640838cc8752' // Replace with your OpenWeatherMap API key
 };
-trackPageView();
-trackVisitor();
